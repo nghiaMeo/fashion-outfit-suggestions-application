e6a0209d0ece8d5340dc4e86796b4f41e85001cb
Nguyen Huu Nghia <nghia181032@gmail.com>
2026-04-22 19:39:08 +0700

    edit gitignore

---
+++ wardrobe-services/src/main/java/com/example/wardrobeservices/WardrobeServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.wardrobeservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardrobeServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WardrobeServicesApplication.class, args);
+    }
+
+}
---
+++ wardrobe-services/src/test/java/com/example/wardrobeservices/WardrobeServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wardrobeservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WardrobeServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
