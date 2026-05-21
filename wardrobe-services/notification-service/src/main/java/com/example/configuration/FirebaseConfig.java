038e2fc2879830d0d6b983669395ba6cdb5f115f
Nguyen Huu Nghia <nghia181032@gmail.com>
2026-05-21 21:44:36 +0700

    chore: ignore firebase service account key

--- wardrobe-services/notification-service/src/main/java/com/example/configuration/FirebaseConfig.java
+++ wardrobe-services/notification-service/src/main/java/com/example/configuration/FirebaseConfig.java
@@ -15,7 +15,7 @@ public class FirebaseConfig {
     @PostConstruct
     public void initialize() {
         try {
-            FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
+            FileInputStream serviceAccount = new FileInputStream("wardrobe-services/serviceAccountKey.json");
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
