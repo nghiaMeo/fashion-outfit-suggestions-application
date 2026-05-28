65bd7cc4a6efbcd10fbade8696c84e1339dda2c9
Nguyen Huu Nghia <nghia181032@gmail.com>
2026-05-28 21:14:03 +0700

    build register_controller

--- fashion_outfit_suggestions_app/lib/app/modules/register/controllers/register_controller.dart
+++ fashion_outfit_suggestions_app/lib/app/modules/register/controllers/register_controller.dart
@@ -1,23 +1,101 @@
+import 'package:fashion_outfit_suggestions_app/core/models/user_response.dart';
+import 'package:fashion_outfit_suggestions_app/core/network/dio_client.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class RegisterController extends GetxController {
-  //TODO: Implement RegisterController
+import '../../../routes/app_routes.dart';
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+class RegisterController extends GetxController {
+  final formKey = GlobalKey<FormState>();
+
+  final emailController = TextEditingController();
+  final usernameController = TextEditingController();
+  final displayNameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  final isLoading = false.obs;
+  final obscurePassword = true.obs;
+
+  final DioClient _dioClient = Get.find<DioClient>();
+
+  Future<void> register() async {
+    if (formKey.currentState!.validate() ?? false) {
+      return;
+    }
+    isLoading.value = true;
+
+    try {
+      final user = await _dioClient.getResult<UserResponse>(
+        _dioClient.dio.post<Map<String, dynamic>>(
+          '/api/auth/register',
+          data: {
+            'email': emailController.text,
+            'username': usernameController.text,
+            'displayName': displayNameController.text,
+            'password': passwordController.text,
+          },
+        ),
+        (json) => UserResponse.fromJson(json! as Map<String, dynamic>),
+      );
+
+      Get.snackbar(
+        'Success',
+        'Register successfully',
+        snackPosition: SnackPosition.BOTTOM,
+      );
+
+      Get.offAllNamed(Routes.login);
+    } on Exception catch (e) {
+      Get.snackbar('Error', e.toString(), snackPosition: SnackPosition.BOTTOM);
+    } catch (_) {
+      Get.snackbar(
+        'Error',
+        'Something went wrong',
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    } finally {
+      isLoading.value = false;
+    }
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  String? validateEmail(String? email) {
+    final value = (email ?? '').trim();
+    if (email!.isEmpty) return ('Email is required');
+
+    if (!GetUtils.isEmail(value)) return ('Invalid email');
+
+    return null;
+  }
+
+  String? validateUsername(String? username) {
+    final value = (username ?? '').trim();
+    if (username!.isEmpty) return ('Username is required');
+    if (username.length < 6) return ('username must at latest 6 characters');
+    return null;
+  }
+
+  String? validateDisplayName(String? displayName) {
+    final value = (displayName ?? '').trim();
+    if (displayName!.isEmpty) return ('Display name is required');
+    if (displayName.length < 8) {
+      return ('display name must at latest 8 characters');
+    }
+    return null;
+  }
+
+  String? validatePassword(String? password) {
+    final value = (password ?? '').trim();
+    if (password!.isEmpty) return ('Password is required');
+    if (password.length < 8) return ('Password must at latest 8 characters');
+    return null;
   }
 
   @override
   void onClose() {
+    emailController.dispose();
+    usernameController.dispose();
+    displayNameController.dispose();
+    passwordController.dispose();
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
