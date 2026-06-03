cf0e4a3081dea3c28fc5e370130406ce335b65a9
Nguyen Huu Nghia <nghia181032@gmail.com>
2026-06-03 16:09:44 +0700

    add: profile controller

--- fashion_outfit_suggestions_app/lib/app/modules/profile/controllers/profile_controller.dart
+++ fashion_outfit_suggestions_app/lib/app/modules/profile/controllers/profile_controller.dart
@@ -1,23 +1,52 @@
+import 'package:fashion_outfit_suggestions_app/core/network/dio_client.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class ProfileController extends GetxController {
-  //TODO: Implement ProfileController
+import '../../../../core/models/user_profile_response.dart';
+import '../../../../core/storage/token_storage.dart';
 
-  final count = 0.obs;
+class ProfileController extends GetxController {
+  final String? targetUserId;
+  final profile = Rxn<UserProfileResponse>();
+  final suggestedUsers = <UserProfileResponse>[].obs;
+  final isLoading = false.obs;
+  final isSuggestedLoading = false.obs;
+  final showSuggestion = false.obs;
+
+  final DioClient _dioClient = Get.find<DioClient>();
+  final TokenStorage _tokenStorage = Get.find<TokenStorage>();
+
+  ProfileController({this.targetUserId});
+
   @override
   void onInit() {
     super.onInit();
+    fetchProfile();
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  Future<void> fetchProfile() async {
+    isLoading.value = true;
+    try {
+      final endpoint = targetUserId != null
+          ? '/api/user/my-profile'
+          : '/api/user/profile/$targetUserId';
+      final response = await _dioClient.getResult<UserProfileResponse>(
+        _dioClient.dio.get(endpoint),
+        (json) => UserProfileResponse.fromJson(json as Map<String, dynamic>),
+      );
+      profile.value = response;
+    } catch (e) {
+      Get.dialog(
+        Dialog(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(e.toString()),
+          ),
+        ),
+        barrierDismissible: false,
+      );
+    } finally {
+      isLoading.value = false;
+    }
   }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
